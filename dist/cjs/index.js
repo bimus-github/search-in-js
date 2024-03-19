a367a90fb38ebcdff5824cb51661869dd481d648
bimus-github <bimus2022@gmail.com>
2024-03-19 11:06:33 +0500

    fixing

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -22,11 +22,12 @@
 function search(props) {
     const { value, query, searchFunction } = props;
     switch (searchFunction) {
-        case "fuzzy":
+        case "fuzzy": {
             return value
                 .toString()
                 .replaceAll(" ", "")
                 .includes(query.toString().replaceAll(" ", ""));
+        }
         case "equal":
             return value.toString() === query.toString();
         case "contains":
@@ -54,5 +55,81 @@ function search(props) {
     }
 }
 
-module.exports = search;
+/**
+ * Searches for a nested property within an object.
+ *
+ * @param {T} obj - the object to search within
+ * @param {string[]} keys - the keys representing the path to the nested property
+ * @param {string} value - the value to search for within the nested property
+ * @param {SEARCH_FUNCTION} functionType - the type of search function to use
+ * @return {boolean} indicates whether the nested property was found
+ */
+function searchNestedProperty(obj, keys, value, functionType) {
+    const [currentKey, ...remainingKeys] = keys;
+    if (obj[currentKey] === undefined)
+        return false;
+    if (remainingKeys.length === 0) {
+        return search({
+            value: obj[currentKey],
+            query: value,
+            searchFunction: functionType,
+        });
+    }
+    if (typeof obj[currentKey] === "object" &&
+        obj[currentKey] !== null) {
+        return searchNestedProperty(obj[currentKey], remainingKeys, value, functionType);
+    }
+    return false;
+}
+
+/**
+ * Hook to search data in an array based on a query using a search function.
+ * @template T The type of data in the array.
+ * @param {string} value The value to search for in the data.
+ * @param {T[]} data The array of data to search.
+ * @param {string[]} keys The array of keys to search for in the data.
+ * @examle
+ * const data: {id: number, name: string, comments: {id: number, text: string}}[] = [
+ *  {id: 1, name: "test1", comments: {id: 1, text: "comment1"}},
+ * ...
+ ]
+ 
+ if you want to search by id and name, you can use the following keys:
+ * const keys = ["id", "name"];
+
+ if you want to search by comments.id and comments.text, you can use the following keys:
+ * const keys = ["comments.id", "comments.text"];
+ * @param {SEARCH_FUNCTION} [functionType="fuzzy"] The type of search function to use.
+ *
+ * @returns { [string, (newValue: string) => void, T[]]} The current value, setter of the search query and the filtered data.
+ *
+ * @example
+ * const [value, setValue] = useState("");
+ * const [filteredData, setFilteredData] = useState(data);
+ *
+ * useEffect(() => {
+ *  setFilteredData(filterData(value, data, keys));
+ * }, [value]);
+ */
+function filterData(value, data, keys, functionType = "fuzzy") {
+    return data.filter((item) => {
+        return keys.some((query) => {
+            if ([...query].find((q) => q === ".")) {
+                const keys = query.split(".");
+                return searchNestedProperty(item, keys, value, functionType);
+            }
+            else {
+                if (item[query] === undefined)
+                    return false;
+                return search({
+                    value: item[query],
+                    query: value,
+                    searchFunction: functionType,
+                });
+            }
+        });
+    });
+}
+
+module.exports = filterData;
 //# sourceMappingURL=index.js.map
